1fb81cf647d38a33ee3bad9bc2264523b1e4a8dc
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2024-11-06 10:25:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-const productRoutes = require('./routes/products');
+const productFetchRoutes = require('./routes/productsFetch'); // New name
+const productSellRoutes = require('./routes/productsSell'); // New name
+const productsBySeller = require('./routes/productsBySeller');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +16,13 @@ app.use(cors());         // Allow cross-origin requests
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api', productRoutes);
+
+app.use('/api', productFetchRoutes);
+
+app.use('/api', productSellRoutes);
+app.use('/api', productsBySeller);
+// Serve static files from the uploads directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/SEM_TEST', { useNewUrlParser: true, useUnifiedTopology: true })
